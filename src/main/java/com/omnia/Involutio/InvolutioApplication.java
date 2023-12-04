36164b461eb492026f787a3dff1f56fc559f8774
vadim2422 <breev.vadim@yandex.ru>
2023-12-04 15:57:47 +0300

    initial project

---
+++ src/main/java/com/omnia/Involutio/InvolutioApplication.java
@@ -0,0 +1,13 @@
+package com.omnia.Involutio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvolutioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvolutioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omnia/Involutio/InvolutioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnia.Involutio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvolutioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
