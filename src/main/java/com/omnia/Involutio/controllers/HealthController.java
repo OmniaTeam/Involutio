6d533966fd7c5fac4b318e992dfe665cb35894f5
YHD97 <ryumkin.ad@gmail.com>
2023-12-07 01:16:09 +0300

    WIP: Add health check

---
+++ src/main/java/com/omnia/Involutio/controllers/HealthController.java
@@ -0,0 +1,18 @@
+package com.omnia.Involutio.controllers;
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthController {
+
+    @GetMapping("/health")
+    ResponseEntity<?> health(){
+        return new ResponseEntity<>("ok", HttpStatusCode.valueOf(200));
+    }
+}
