e09ea33ce1baca6404a693d9cbeca380af4f0f25
YHD97 <ryumkin.ad@gmail.com>
2023-12-11 00:22:53 +0300

    FIX: Security routing

--- src/main/java/com/omnia/Involutio/security/config/SecurityConfiguration.java
+++ src/main/java/com/omnia/Involutio/security/config/SecurityConfiguration.java
@@ -40,6 +40,8 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.GET,"/api-docs/**").permitAll()
                         .requestMatchers("/manager/**").hasAuthority(ERole.MANAGER.name())
                         .requestMatchers("/health").permitAll()
+                        .requestMatchers("/files/**").hasAnyAuthority()
+                        .requestMatchers("/worker/**").hasAnyAuthority()
 
 //                        .requestMatchers("/user").hasAuthority(Role.USER.name())
                         .anyRequest().authenticated())
