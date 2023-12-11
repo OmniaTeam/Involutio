20de7936506a9ec67da82c5ffd262c05245285d8
YHD97 <ryumkin.ad@gmail.com>
2023-12-11 23:24:22 +0300

    WIP: CSV Parser

---
+++ src/main/java/com/omnia/Involutio/service/file/CSVReaderMaster.java
@@ -0,0 +1,34 @@
+package com.omnia.Involutio.service.file;
+
+import com.omnia.Involutio.entity.CSVEntity;
+import com.omnia.Involutio.repository.CSVRepository;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class CSVReaderMaster {
+    private final CSVRepository csvRepository;
+    private final CSVComponent csvComponent;
+
+    public CSVReaderMaster(CSVRepository csvRepository, CSVComponent csvComponent) {
+        this.csvRepository = csvRepository;
+        this.csvComponent = csvComponent;
+    }
+    //TODO: mental magic
+    public List<CSVEntity> read(String filePath) throws IOException {
+        try {
+            var list = csvComponent.parseCsv(filePath);
+            return csvRepository.saveAll(list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
+
+
+}
