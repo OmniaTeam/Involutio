ff38060e520f187bf07db406e424d460fa2f2e47
YHD97 <ryumkin.ad@gmail.com>
2023-12-12 13:12:36 +0300

    WIP: UserController

---
+++ src/main/java/com/omnia/Involutio/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.omnia.Involutio.controllers;
+
+import com.omnia.Involutio.entity.UserEntity;
+import com.omnia.Involutio.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    final private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    /*
+    Получение данных пользователя через аутентификацию
+     */
+    @GetMapping("/")
+    ResponseEntity<?> getUserWithAuthentication(Authentication authentication){
+    return ResponseEntity.ok(((UserEntity) authentication.getPrincipal()));
+    }
+
+    /*
+    Получение данных пользователя через id
+     */
+    @GetMapping("/{userId}")
+    ResponseEntity<?> getUserWithId(@PathVariable Long userId){
+        return ResponseEntity.ok(userService.getUserById(userId));
+    }
+}
