befe063ec42e5f5d38a8127f6540252bf69b2152
YHD97 <ryumkin.ad@gmail.com>
2023-12-13 01:26:34 +0300

    EnableScheduling

--- src/main/java/com/omnia/Involutio/InvolutioApplication.java
+++ src/main/java/com/omnia/Involutio/InvolutioApplication.java
@@ -3,10 +3,11 @@ package com.omnia.Involutio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaRepositories
-
+@EnableScheduling
 public class InvolutioApplication {
 
 	public static void main(String[] args) {
