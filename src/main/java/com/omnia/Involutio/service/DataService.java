9ce00ab13c45f4fc5af066232138896dd426f374
vadim2422 <breev.vadim@yandex.ru>
2023-12-13 00:11:34 +0300

    WIP: add DataService

---
+++ src/main/java/com/omnia/Involutio/service/DataService.java
@@ -0,0 +1,12 @@
+package com.omnia.Involutio.service;
+
+import com.omnia.Involutio.entity.CSVEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataService {
+
+    public double calcIndex(CSVEntity csvEntity) {
+        return 20;
+    }
+}
