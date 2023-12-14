568ee3f6cac1b0f89d8feeab7f98244b1dfa4a0a
vadim2422 <breev.vadim@yandex.ru>
2023-12-15 00:48:19 +0300

    WIP: analysis logic

--- src/main/java/com/omnia/Involutio/service/DataService.java
+++ src/main/java/com/omnia/Involutio/service/DataService.java
@@ -1,15 +1,19 @@
 package com.omnia.Involutio.service;
 
+import com.omnia.Involutio.dto.DataDTO;
 import com.omnia.Involutio.ecxeptions.NotFoundException;
 import com.omnia.Involutio.entity.CSVEntity;
 import com.omnia.Involutio.entity.WorkerEntity;
 import com.omnia.Involutio.entity.WorkerRatingEntity;
+import com.omnia.Involutio.repository.CSVRepository;
 import com.omnia.Involutio.repository.WorkerRatingRepository;
 import com.omnia.Involutio.repository.WorkerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Random;
 
 @Service
@@ -18,6 +22,7 @@ import java.util.Random;
 public class DataService {
 
     private final WorkerMaster workerMaster;
+    private final CSVRepository csvRepository;
     private final WorkerRepository workerRepository;
     private final WorkerRatingRepository workerRatingRepository;
 
@@ -32,7 +37,7 @@ public class DataService {
             return;
         }
 
-        int rating = calcRating(csvEntity);
+        double rating = calcRating(csvEntity);
         WorkerRatingEntity workerRatingEntity = new WorkerRatingEntity(rating, worker.getId());
         workerRatingRepository.save(workerRatingEntity);
         worker.setRating((double) rating);
@@ -40,10 +45,19 @@ public class DataService {
 
     }
 
-    private int calcRating(CSVEntity csvEntity) {
-        Random random = new Random();
-        int rating = random.nextInt(71) + 10;
-        return rating;
+    private double calcRating(CSVEntity csvEntity) {
+        return DataDTO.calculateEmployeeAttritionProbability(csvEntity);
+//        Random random = new Random();
+//        int rating = random.nextInt(71) + 10;
+//        return rating;
+    }
+
+    @Bean
+    private void test() {
+        List<CSVEntity> entityList =  csvRepository.findAll();
+        for (CSVEntity entity: entityList) {
+            System.out.println(calcRating(entity));
+        }
     }
 
 }
