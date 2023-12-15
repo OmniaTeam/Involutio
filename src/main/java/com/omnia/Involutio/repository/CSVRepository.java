81f3c11197c8593fc6e83afd316992139da72fad
vadim2422 <breev.vadim@yandex.ru>
2023-12-15 23:59:03 +0300

    FIX: repeat data

--- src/main/java/com/omnia/Involutio/repository/CSVRepository.java
+++ src/main/java/com/omnia/Involutio/repository/CSVRepository.java
@@ -4,7 +4,10 @@ import com.omnia.Involutio.entity.CSVEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface CSVRepository extends JpaRepository<CSVEntity, Long> {
 
+    Boolean existsByEmailIgnoreCaseAndDate(String email, LocalDate date);
 }
--- src/main/java/com/omnia/Involutio/service/file/CSVReaderMaster.java
+++ src/main/java/com/omnia/Involutio/service/file/CSVReaderMaster.java
@@ -3,24 +3,28 @@ package com.omnia.Involutio.service.file;
 import com.omnia.Involutio.dto.CSVDTO;
 import com.omnia.Involutio.entity.CSVEntity;
 import com.omnia.Involutio.repository.CSVRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CSVReaderMaster {
     private final CSVRepository csvRepository;
     private final CSVComponent csvComponent;
 
-    public CSVReaderMaster(CSVRepository csvRepository, CSVComponent csvComponent) {
-        this.csvRepository = csvRepository;
-        this.csvComponent = csvComponent;
-    }
     //TODO: mental magic
     public List<CSVEntity> read(String filePath) throws IOException {
         try {
-            var list = csvComponent.parseCsv(filePath).stream().map(CSVEntity::new).toList();
+            var list = csvComponent.parseCsv(filePath).stream()
+                    .filter((csvdto) -> !csvRepository.existsByEmailIgnoreCaseAndDate(
+                            csvdto.getEmail(),
+                            LocalDate.now()))
+                    .map(CSVEntity::new).toList();
+
             csvRepository.saveAll(list);
             return list;
         } catch (IOException e) {
